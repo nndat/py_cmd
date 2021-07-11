23de45a95a83b886f6e2e44159d4ef7473baa8f2
nndat <nguyenngocdat90@gmail.com>
2021-07-11 14:28:02 +0700

    add cat cmd

---
+++ cat.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+"""
+Implement `cat` command with python code.
+"""
+
+import click
+
+
+@click.command()
+@click.argument('files', type=click.File('r'), nargs=-1)
+@click.option('-n', '--number', is_flag=True)
+def cat_cmd(files, number):
+    line_count = 1
+    for _file in files:
+        for line in _file:
+            show_line = line_count if number else ""
+            click.echo(f"{show_line:<5}  {line}", nl=False)
+            line_count += 1
+
+
+if __name__ == "__main__":
+    cat_cmd()
